87381098449d80f22b9bf8b8bf91d6426857bb34
Alex Tebbs <alex@alextebbs.com>
2023-04-07 14:16:03 -0700

    plugboard ui v1

--- src/components/dom/Plugboard/Plugboard.tsx
+++ src/components/dom/Plugboard/Plugboard.tsx
@@ -1,5 +1,5 @@
 import { KeyboardLayout } from '@/_globals'
-import { useEffect, useState } from 'react'
+import { useRef, useEffect, useState, forwardRef } from 'react'
 
 type Point = [x: number, y: number]
 
@@ -9,53 +9,130 @@ type Point = [x: number, y: number]
         3. Hovering over an active item should highlight it's partner
 */
 
-const canvasConfig = [
-  {
-    from: 'A',
-    to: 'B',
-  },
-  {
-    from: 'C',
-    to: 'D',
-  },
-  {
-    from: 'I',
-    to: 'Y',
-  },
-]
-
 const getDistance = (x1, y1, x2, y2) => {
   let y = x2 - x1
   let x = y2 - y1
   return Math.sqrt(x * x + y * y)
 }
 
+const getUniquePairings = (plugboardState) => {
+  const uniquePairs = {}
+
+  for (const key in plugboardState) {
+    const value = plugboardState[key]
+
+    if (!(value in uniquePairs) || uniquePairs[value] !== key) {
+      uniquePairs[key] = value
+    }
+  }
+
+  return uniquePairs
+}
+
+interface PlugboardKeyProps {
+  active: boolean
+  char: string
+  hovered: boolean
+  onPlugboardKeyClick: Function
+  onPlugboardKeyMouseEnter: Function
+  onPlugboardKeyMouseLeave: Function
+}
+
+const PlugboardKey = forwardRef<HTMLDivElement, PlugboardKeyProps>(function PlugboardKey(
+  { active, char, hovered, onPlugboardKeyClick, onPlugboardKeyMouseEnter, onPlugboardKeyMouseLeave },
+  ref,
+) {
+  let keyStateClasses = 'text-green-500 border-green-500'
+  let dotStateClasses = 'border-green-500'
+
+  if (hovered) {
+    keyStateClasses = 'text-blue-500 border-blue-500'
+    dotStateClasses = 'border-blue-500'
+  }
+  if (active) {
+    keyStateClasses = 'text-white border-white'
+    dotStateClasses = 'bg-white border-white'
+  }
+  if (active && hovered) {
+    keyStateClasses = 'text-red-500 border-red-500'
+    dotStateClasses = 'bg-red-500 border-red-500'
+  }
+
+  return (
+    <span
+      ref={ref}
+      data-key={char}
+      onClick={(e) => onPlugboardKeyClick(e)}
+      onMouseEnter={(e) => onPlugboardKeyMouseEnter(e)}
+      onMouseLeave={(e) => onPlugboardKeyMouseLeave(e)}
+      className={`plugboardKey hover:cursor-pointer inline-block px-3 mx-2 py-2 pb-3 rounded-lg uppercase font-bold border font-mono ${keyStateClasses}`}>
+      {char.toLowerCase()}
+      <div className={`rounded-full w-3 h-3 border-2 mt-2 ${dotStateClasses}`}></div>
+    </span>
+  )
+})
+
 export default function Plugboard() {
-  useEffect(() => {
-    const c = document.getElementById('plugboardCanvas') as HTMLCanvasElement
-    c.style.width = '100%'
-    c.style.height = '100%'
-    c.width = c.offsetWidth
-    c.height = c.offsetHeight
+  console.log('RENDERING')
 
-    drawPlugboardState(canvasConfig, c.getContext('2d'), c)
-  })
+  const [plugboardState, setPlugboardState] = useState({})
+  const [workingKey, setWorkingKey] = useState('')
+  const [hoveredKey, setHoveredKey] = useState('')
 
-  const [isEditing, setIsEditing] = useState(false)
+  const isEditing = useRef(false)
+  const workingBezierStart = useRef(null)
+  const canvasRect = useRef(null)
 
-  const drawPlugboardState = (canvasConfig, ctx, c) => {
-    canvasConfig.forEach((cfgItem) => {
-      const startEl = document.querySelector(`[data-key="${cfgItem.from}"]`)
-      const endEl = document.querySelector(`[data-key="${cfgItem.to}"]`)
+  const keysRef = useRef(null)
 
-      ;[startEl, endEl].forEach((el) => {
-        el.children[0].classList.add('bg-white')
+  // const workingKey = useRef('')
+
+  const plugboard = useRef(null)
+  const canvas = useRef(null)
+  const workingCanvas = useRef(null)
+
+  function getMap() {
+    if (!keysRef.current) {
+      // initialize the Map on first usage.
+      keysRef.current = new Map()
+    }
+    return keysRef.current
+  }
+
+  useEffect(
+    () => {
+      console.log('doing useeffect things')
+      ;[canvas.current, workingCanvas.current].forEach((el) => {
+        el.style.width = '100%'
+        el.style.height = '100%'
+        el.width = el.offsetWidth
+        el.height = el.offsetHeight
       })
+
+      canvasRect.current = workingCanvas.current.getBoundingClientRect()
+
+      drawPlugboardState(plugboardState)
+    } /* Why does this not work? [plugboardState] */,
+  )
+
+  const clearWorkingCanvas = () => {
+    const ctx = workingCanvas.current.getContext('2d')
+    ctx.clearRect(0, 0, workingCanvas.current.width, workingCanvas.current.height)
+  }
+
+  const drawPlugboardState = (plugboardState) => {
+    const uniquePairs = getUniquePairings(plugboardState)
+
+    console.log('rendering plugboard state')
+
+    Object.entries(uniquePairs).forEach((item) => {
+      const startEl = document.querySelector(`[data-key="${item[0]}"]`) as HTMLElement
+      const endEl = document.querySelector(`[data-key="${item[1]}"]`) as HTMLElement
 
       const startRect = startEl.children[0].getBoundingClientRect()
       const endRect = endEl.children[0].getBoundingClientRect()
 
-      const cRect = c.getBoundingClientRect()
+      const cRect = canvas.current.getBoundingClientRect()
 
       const bezierStart: Point = [
         startRect.left - cRect.left + startRect.width / 2,
@@ -67,99 +144,126 @@ export default function Plugboard() {
         endRect.top - cRect.top + endRect.height / 2,
       ]
 
-      let bezierControlPoint: Point = [(bezierStart[0] + bezierEnd[0]) / 2, (bezierStart[1] + bezierEnd[1]) / 2 + 120]
+      let color = startEl.dataset.key == hoveredKey || endEl.dataset.key == hoveredKey ? 'red' : 'white'
 
-      drawBezier(ctx, bezierStart, bezierControlPoint, bezierEnd)
+      drawBezier(canvas.current.getContext('2d'), bezierStart, bezierEnd, color)
     })
   }
 
-  const handlePlugboardMouseEnter = (e) => {}
-
-  const drawBezier = (ctx: CanvasRenderingContext2D, start: Point, mid: Point, end: Point) => {
-    ctx.strokeStyle = 'white'
-    ctx.lineWidth = 2
+  const drawBezier = (ctx: CanvasRenderingContext2D, start: Point, end: Point, color: string) => {
+    // const distanceOffset = (1 / getDistance(...start, ...end)) * 500
+    const distanceOffset = 50
+    const mid: Point = [(start[0] + end[0]) / 2, (start[1] + end[1]) / 2 + distanceOffset]
+    ctx.strokeStyle = color
+    ctx.lineWidth = 3
     ctx.beginPath()
     ctx.moveTo(...start)
     ctx.quadraticCurveTo(...mid, ...end)
     ctx.stroke()
   }
 
-  const handlePlugboardMouseMove = (e: MouseEvent, c, ctx, cRect, bezierStart) => {
-    ctx.clearRect(0, 0, c.width, c.height)
-    let bezierEnd: Point = [e.clientX - cRect.left, e.clientY - cRect.top]
-    let bezierControlPoint: Point = [(bezierStart[0] + bezierEnd[0]) / 2, (bezierStart[1] + bezierEnd[1]) / 2 + 120]
-    drawBezier(ctx, bezierStart, bezierControlPoint, bezierEnd)
-  }
+  const onPlugboardKeyClick = (e: MouseEvent) => {
+    const thisKey = e.currentTarget as HTMLElement
 
-  const handlePlugboardClick = (e) => {
-    if (isEditing) {
-      setIsEditing(false)
+    console.log('clicked on', thisKey.dataset.key)
+
+    if (isEditing.current) {
+      isEditing.current = false
+
+      if (thisKey.dataset.key == workingKey || thisKey.dataset.key in plugboardState) {
+        clearWorkingCanvas()
+        setWorkingKey('')
+        return
+      }
+
+      setWorkingKey('')
+      const newPlugboardState = plugboardState
+      newPlugboardState[workingKey] = thisKey.dataset.key
+      newPlugboardState[thisKey.dataset.key] = workingKey
+      setPlugboardState(newPlugboardState)
       return
     }
 
-    setIsEditing(true)
+    if (thisKey.dataset.key in plugboardState) {
+      console.log('we clicked on something that was already set')
 
-    const c = document.getElementById('plugboardCanvas') as HTMLCanvasElement
-    const plugboard = document.getElementById('plugboard')
-    const ctx = c.getContext('2d')
+      const newPlugboardState = plugboardState
+      delete newPlugboardState[newPlugboardState[thisKey.dataset.key]]
+      delete newPlugboardState[thisKey.dataset.key]
+      setPlugboardState(newPlugboardState)
+    }
 
-    const thisKey = e.currentTarget
-    const thisDot = thisKey.children[0]
+    setWorkingKey(thisKey.dataset.key)
 
-    thisDot.classList.add('bg-white')
-    thisKey.classList.add('bg-green-900')
+    const dotRect = thisKey.children[0].getBoundingClientRect()
 
-    const dotRect = thisDot.getBoundingClientRect()
-    const cRect = c.getBoundingClientRect()
-
-    const bezierStart: Point = [
-      dotRect.left - cRect.left + dotRect.width / 2,
-      dotRect.top - cRect.top + dotRect.height / 2,
+    workingBezierStart.current = [
+      dotRect.left - canvasRect.current.left + dotRect.width / 2,
+      dotRect.top - canvasRect.current.top + dotRect.height / 2,
     ]
 
-    // Dunno why this doesn't work
-    // let bezierEnd: Point = [e.clientX - cRect.left, e.clientY - cRect.top]
-    // let bezierControlPoint: Point = [(bezierStart[0] + bezierEnd[0]) / 2, (bezierStart[1] + bezierEnd[1]) / 2 + 120]
-    // drawBezier(ctx, bezierStart, bezierControlPoint, bezierEnd)
+    isEditing.current = true
+  }
 
-    const handlePlugboardMouseMoveWrapper = (event) => handlePlugboardMouseMove(event, c, ctx, cRect, bezierStart)
+  const onPlugboardMouseMove = (e) => {
+    if (!isEditing.current) return
 
-    plugboard.addEventListener('mousemove', handlePlugboardMouseMoveWrapper)
+    clearWorkingCanvas()
 
-    const keys = document.querySelectorAll('.plugboardKey')
+    drawBezier(
+      workingCanvas.current.getContext('2d'),
+      workingBezierStart.current,
+      [e.clientX - canvasRect.current.left, e.clientY - canvasRect.current.top],
+      'white',
+    )
+  }
 
-    keys.forEach((key) => {
-      key.addEventListener('click', (e) => {
-        plugboard.removeEventListener('mousemove', handlePlugboardMouseMoveWrapper)
-        const thisTarget = e.currentTarget as Element
+  const onPlugboardKeyMouseEnter = (e) => {
+    const thisKey = e.currentTarget as HTMLElement
+    setHoveredKey(thisKey.dataset.key)
+  }
 
-        thisDot.classList.remove('bg-white')
-        thisKey.classList.remove('bg-green-900')
-      })
-    })
+  const onPlugboardKeyMouseLeave = (e) => {
+    setHoveredKey('')
   }
 
   return (
     <div className='flex align-center justify-center'>
       <div className='relative'>
         <div className={`absolute w-full h-full pointer-events-none`}>
-          <canvas id='plugboardCanvas'></canvas>
+          <canvas ref={canvas} id='plugboardCanvas'></canvas>
         </div>
-        <div id='plugboard' className='py-5'>
+        <div className={`absolute w-full h-full pointer-events-none`}>
+          <canvas ref={workingCanvas} id='plugboardWorkingCanvas'></canvas>
+        </div>
+
+        <div id='plugboard' ref={plugboard} onMouseMove={(e) => onPlugboardMouseMove(e)} className='p-20 pt-10'>
+          <div className='font-mono uppercase tracking-[.25em] text-xs text-center text-zinc-400 pb-1'>Plugboard</div>
+          <div className='font-mono text-xs text-center text-zinc-400 pb-10'>
+            Click on letters to connect them with wires.
+          </div>
           {KeyboardLayout.map((row) => {
             return (
               <div className='text-center mb-3' key={row}>
                 {row.split('').map((char) => {
                   return (
-                    <span
+                    <PlugboardKey
                       key={char}
-                      data-key={char}
-                      onClick={(e) => handlePlugboardClick(e)}
-                      onMouseEnter={(e) => handlePlugboardMouseEnter(e)}
-                      className='plugboardKey inline-block px-3 mx-2 py-2 pb-3 rounded-sm lowercase border font-mono text-green-500 border-green-500 hover:cursor-pointer'>
-                      {char.toLowerCase()}
-                      <div className='rounded-full w-3 h-3 border-2 border-green-500 mt-2'></div>
-                    </span>
+                      ref={(node) => {
+                        const map = getMap()
+                        if (node) {
+                          map.set(char, node)
+                        } else {
+                          map.delete(char)
+                        }
+                      }}
+                      char={char}
+                      active={char in plugboardState || workingKey === char ? true : false}
+                      hovered={hoveredKey === char || plugboardState[char] === hoveredKey}
+                      onPlugboardKeyClick={onPlugboardKeyClick}
+                      onPlugboardKeyMouseEnter={onPlugboardKeyMouseEnter}
+                      onPlugboardKeyMouseLeave={onPlugboardKeyMouseLeave}
+                    />
                   )
                 })}
               </div>
--- src/components/dom/Plugboard/PlugboardKey.tsx
+++ src/components/dom/Plugboard/PlugboardKey.tsx
@@ -4,13 +4,16 @@ import { useEffect, useState } from 'react'
 export default function PlugboardKey(props) {
   return (
     <span
-      key={props.char}
       data-key={props.char}
-      onClick={(e) => props.handlePlugboardClick(e)}
-      onMouseEnter={(e) => props.handlePlugboardMouseEnter(e)}
-      className='plugboardKey inline-block px-3 mx-2 py-2 pb-3 rounded-sm lowercase border font-mono text-green-500 border-green-500 hover:cursor-pointer'>
+      onClick={(e) => props.onPlugboardKeyClick(e)}
+      className={`plugboardKey hover:cursor-pointer inline-block px-3 mx-2 py-2 pb-3 rounded-sm lowercase border font-mono ${
+        props.active ? 'text-white border-white' : 'text-green-500 border-green-500'
+      }`}>
       {props.char.toLowerCase()}
-      <div className='rounded-full w-3 h-3 border-2 border-green-500 mt-2'></div>
+      <div
+        className={`rounded-full w-3 h-3 border-2 mt-2 ${
+          props.active ? 'bg-white border-white' : 'border-green-500'
+        }`}></div>
     </span>
   )
 }
